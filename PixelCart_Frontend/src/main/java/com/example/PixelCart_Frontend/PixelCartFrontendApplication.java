c2f3e16969fe0f6aa3ddb07322a973102c5fc490
Smit Kadam <smitkadam148@gmail.com>
2026-01-09 15:27:55 +0530

    Create PixelCartFrontendApplication.java

---
+++ PixelCart_Frontend/src/main/java/com/example/PixelCart_Frontend/PixelCartFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.PixelCart_Frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelCartFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelCartFrontendApplication.class, args);
+	}
+
+}
