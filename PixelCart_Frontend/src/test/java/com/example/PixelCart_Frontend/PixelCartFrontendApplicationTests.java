489e5663df44ae2624589b1b89388beab1af7fe6
Smit Kadam <smitkadam148@gmail.com>
2026-01-09 15:31:57 +0530

    1

---
+++ PixelCart_Frontend/src/test/java/com/example/PixelCart_Frontend/PixelCartFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PixelCart_Frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelCartFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ templates/src/main/java/com/example/templates/PageController.java
@@ -0,0 +1,6 @@
+package com.example.templates;
+
+public class PageController {
+    
+    @GetMapp
+}
