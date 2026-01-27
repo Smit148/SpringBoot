64c6debaaff20fa8c1afe94ca91722cf69584f7c
Smit Kadam <smitkadam148@gmail.com>
2026-01-27 18:35:36 +0530

    Create UrlHandlingApplicationTests.java

---
+++ url_handling/src/test/java/com/example/url_handling/UrlHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.url_handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
