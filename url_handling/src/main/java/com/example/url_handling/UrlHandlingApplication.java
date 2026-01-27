d113555271f4fe314ccb4c519a39702c106dae00
Smit Kadam <smitkadam148@gmail.com>
2026-01-27 18:36:52 +0530

    Create UrlHandlingApplication.java

---
+++ url_handling/src/main/java/com/example/url_handling/UrlHandlingApplication.java
@@ -0,0 +1,14 @@
+package com.example.url_handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlHandlingApplication.class, args);
+	}
+
+}
+ 
